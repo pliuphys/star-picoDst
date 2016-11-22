9fa93d521801210aa91feb9efdb8bf19d7f7026f
Lukasz Fulek <lukfulek@rcas6007.rcf.bnl.gov>
2016-11-22 04:48:44 -0500

    BTof vertex selection

--- makePicoDst.C
+++ makePicoDst.C
@@ -10,7 +10,7 @@ class StMuDstMaker;
 StChain* chain;
 void makePicoDst(const Char_t *inputFile = "/star/data05/scratch/lukful/muDST/st_rp_16106026_raw_2000001.MuDst.root"/*"root://xrdstar.rcf.bnl.gov:1095//home/starlib/home/starreco/reco/AuAu_200_production_low_2014/ReversedFullField/P15ic/2014/140/15140004/st_physics_15140004_raw_1000016.MuDst.root"*/)
 {
-  Int_t nEvents = 100;//1e5;
+  Int_t nEvents = 1e5;
 
   gSystem->Load("libTable");
   gSystem->Load("libPhysics");
